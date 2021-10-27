696cca9c864d188fbc4d5ecbd46e8c4f94f14e97
shiam <shiamabdullah1@gmail.com>
2021-10-27 06:18:27 +0600

    todo models created

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from todo.models import Todo
+
+class TodoAdmin(admin.ModelAdmin):
+    readonly_fields = ('createdAt',)
+
 # Register your models here.
+admin.site.register(Todo, TodoAdmin)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Todo (models.Model):
+    title= models.CharField(max_length=100)
+    memo= models.TextField(blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    completedAt = models.DateTimeField(null=True, blank=True)
+    important = models.BooleanField(default=False)
+    todoCreator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
